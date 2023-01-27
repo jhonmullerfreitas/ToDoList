4ebcfef6da33fdac8bc6ae4302f025c6c831c301
Jhon Muller <jmuller.jhon80@gmail.com>
2023-01-27 11:17:08 -0400

    feat: autorização para CRUD de lista através de token.

--- api/src/controllers/ListController.js
+++ api/src/controllers/ListController.js
@@ -4,25 +4,29 @@ export class ListController {
 
     static create = (request, response) => {
         const {name} = request.body
-        const list = ListService.create(name)
+        const token = request.headers.authorization
+        const list = ListService.create(name, token)
         return response.json(list)
     }
 
     static readAll = (request, response) => {
-        const lists = ListService.readAll()
+        const token = request.headers.authorization
+        const lists = ListService.readAll(token)
         return response.json(lists)
     }
 
     static updateList = (request, response) => {
         const {id} = request.params
         const {name} = request.body
-        const listUpdated = ListService.updateList(id, name)
+        const token = request.headers.authorization
+        const listUpdated = ListService.updateList(id, name, token)
         return response.json(listUpdated)
     }
 
     static deleteList = (request, response) => {
         const {id} = request.params
-        const listDeleted = ListService.deleteList(id)
+        const token = request.headers.authorization
+        const listDeleted = ListService.deleteList(id, token)
         return response.json(listDeleted)
     }
 
